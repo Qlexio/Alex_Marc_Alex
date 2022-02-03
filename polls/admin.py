a2efbbcad3ced513da9cd6cc09f983b6ab32784b
NMarcR42 <94183312+NMarcR42@users.noreply.github.com>
2022-02-03 18:15:35 +0100

    coreection sql. Script js non fonctionnel

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-# from .models import Articles
+from .models import Reference, Type_reference, Diametre_corps, Longueur_corps, Liste_produit
 
 
-# admin.site.register(Articles)
+admin.site.register(Reference)
+admin.site.register(Type_reference)
+admin.site.register(Diametre_corps)
+admin.site.register(Longueur_corps)
+admin.site.register(Liste_produit)
+
